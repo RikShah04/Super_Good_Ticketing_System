4bb45ccc54b7ad7a1f82bcd561805ffe9dad4206
jmccormack2703 <caleighmccor@umass.edu>
2026-04-14 10:49:40 -0400

    fix: modified service to have consistent naming conventions, restarted docker volume. Error seems to be corrected.

--- event-catalog/server.js
+++ event-catalog/server.js
@@ -7,7 +7,7 @@ const port = Number(process.env.PORT || '3005');
 const SERVICE_NAME = process.env.SERVICE_NAME || 'event-catalog';
 const events = {};
 
-const DATABASE_URL = process.env.DATABASE_URL || "postgres://user:pass@events-db:5432/events"
+const DATABASE_URL = process.env.DATABASE_URL || "postgres://user:pass@events-db:5432/events-db"
 const redisUrl = process.env.REDIS_URL || 'redis://redis:6379';
 
 const pool = new pg.Pool({ connectionString: DATABASE_URL });
