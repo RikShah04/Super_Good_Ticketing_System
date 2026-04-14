e6e3f7a9ffa1f8e994c31743fa7e5b299492ed61
jmccormack2703 <caleighmccor@umass.edu>
2026-04-13 22:42:31 -0400

    feat: incorporated pool and pg into server.js

--- event-catalog/server.js
+++ event-catalog/server.js
@@ -1,11 +1,19 @@
-import express from 'express'
-import redis from 'redis'
+import express from 'express';
+import redis from 'redis';
+import pg from 'pg';
 
 const app = express();
 const port = Number(process.env.PORT || '8000');
 const events = {};
 
+const DATABASE_URL = process.env.DATABASE_URL || "postgres://user:pass@events-db:5432/events"
 const redisUrl = process.env.REDIS_URL || 'redis://redis:6379';
+
+const pool = new pg.Pool({ connectionString: DATABASE_URL });
+
+pool.on('error', err => {
+    console.error('Postgres error: ', err.message);
+});
 
 const client = redis.createClient({ url: redisUrl });
 
