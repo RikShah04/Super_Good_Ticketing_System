848c129a3e22ab354ce9df719d0ad77a1b60d202
jmccormack2703 <caleighmccor@umass.edu>
2026-04-14 10:40:17 -0400

    fix: attempting to correct bug where "db does not exist"

--- event-catalog/server.js
+++ event-catalog/server.js
@@ -3,18 +3,18 @@ import redis from 'redis';
 import pg from 'pg';
 
 const app = express();
-const port = Number(process.env.PORT || '8000');
+const port = Number(process.env.PORT || '3005');
 const SERVICE_NAME = process.env.SERVICE_NAME || 'event-catalog';
 const events = {};
 
-//const DATABASE_URL = process.env.DATABASE_URL || "postgres://user:pass@events-db:5432/events"
+const DATABASE_URL = process.env.DATABASE_URL || "postgres://user:pass@events-db:5432/events"
 const redisUrl = process.env.REDIS_URL || 'redis://redis:6379';
 
-//const pool = new pg.Pool({ connectionString: DATABASE_URL });
+const pool = new pg.Pool({ connectionString: DATABASE_URL });
 
-//pool.on('error', err => {
-//    console.error('Postgres error: ', err.message);
-//});
+pool.on('error', err => {
+    console.error('Postgres error: ', err.message);
+});
 
 const client = redis.createClient({ url: redisUrl });
 
