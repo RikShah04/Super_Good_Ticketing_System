4f1ae18f1dacc15a1fef8269c217ec544fdf8181
jmccormack2703 <caleighmccor@umass.edu>
2026-04-16 10:32:02 -0400

    feat: implemented a fake event generator function, refactored database schema to include 'totalSeats'.

--- event-catalog/db/schema.sql
+++ event-catalog/db/schema.sql
@@ -1,10 +1,10 @@
 -- create "eventCatalog" table
 CREATE TABLE eventCatalog (
-    id varchar(100) NOT NULL,
+    id varchar(100) PRIMARY KEY,
     name varchar(100) NOT NULL,
     venue varchar(100) NOT NULL,
     eventDate DATE NOT NULL,
+    totalSeats int CHECK (totalSeats > 0),
     availableSeats int NOT NULL,
-    priceUsd DECIMAL(10. 2) NULL,
-    PRIMARY KEY (id)
+    priceUsd DECIMAL(10, 2) CHECK (priceUsd >= 0),
 );
