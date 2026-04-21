f13415cf9762ba6ea59a33533697f76f002d8e64
jmccormack2703 <caleighmccor@umass.edu>
2026-04-21 11:03:57 -0400

    feat: create a 'user' table in schema.sql

--- users/db/schema.sql
+++ users/db/schema.sql
@@ -1,1 +1,7 @@
--- TODO: Database schema here
+-- create "users" table
+CREATE TABLE user (
+    userid varchar(100) PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    email varchar(100) UNIQUE NOT NULL,
+    created_at timestamp DEFAULT NOW()
+)
