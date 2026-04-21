04c3760eb6e8fb3c8552f1bedc373cf549574ee6
jmccormack2703 <caleighmccor@umass.edu>
2026-04-20 21:19:02 -0400

    feat: improved pagination of database and redis retrieval for events endpoint.

--- event-catalog/server.js
+++ event-catalog/server.js
@@ -26,7 +26,7 @@ await client.connect();
 
 app.use(express.json());
 
-app.get('/events', (req, res) => {
+app.get('/events', async (req, res) => {
     // Default page is 1
     const page = Math.max(parseInt(req.query.page || "1", 10), 1);
     // Default is 20, hard limit at 100.
@@ -46,50 +46,47 @@ app.get('/events', (req, res) => {
         const cached = await client.get(cacheKey);
 
         if (cached) {
-            return res.json({
-                page,
-                limit,
-                total,
-                events: JSON.parse(cached),
-            });
+            return res.json(JSON.parse(cached));
         }
 
         // Not in redis? query the db
-        let query = `SELECT * FROM eventcatalog`;
+        let baseQuery = `FROM eventcatalog`;
         const params = [];
         const conditions = [];
 
         if (venue) {
             params.push(`%${venue}%`);
-            conditions.push(`venue ILIKE ${params.length}`);
+            conditions.push(`venue ILIKE $${params.length}`);
         }
 
         if (conditions.length > 0) {
-            query += " WHERE " + conditions.join(" AND ");
+            baseQuery += " WHERE " + conditions.join(" AND ");
         }
 
+        // Count query (exclude limit + offset)
+        const countResult = await pool.query(`SELECT COUNT(*) ${baseQuery}`, params);
+        const total = parseInt(countResult.rows[0].count, 10);
+
+        // Data query (include limit + offset)
         params.push(limit);
         params.push(offset);
 
-        query += `ORDER BY eventdate ASC LIMIT $${params.length - 1} OFFSET $${params.length}`;
+        const dataQuery = `SELECT * ${baseQuery} ORDER BY eventdate ASC LIMIT $${params.length - 1} OFFSET $${params.length}`;
 
-        const result = await pool.query(query, params);
+        const result = await pool.query(dataQuery, params);
 
         const rows = result.rows;
 
-        // Cache results in redis
-        await client.setEx(
-            cacheKey,
-            60,
-            JSON.stringify(result.rows)
-        );
-
-        res.json({
+        const response = {
             page,
             limit,
-            total: rows.length,
+            total,
             events: result.rows,
-        });
+        };
+        // Cache results in redis
+        await client.setEx(cacheKey, 60, JSON.stringify(response));
+
+        res.json(response);
 
     } catch (err) {
         console.error("GET /events failed:", err);
