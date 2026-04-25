2a8b632596b994e721cc6eea7efa2daef1d45939
jmccormack2703 <caleighmccor@umass.edu>
2026-04-25 16:06:28 -0400

    feat: implemented GET /users endpoint with Redis caching

--- users/server.js
+++ users/server.js
@@ -7,6 +7,7 @@ const port = Number(process.env.PORT || '3006');
 const SERVICE_NAME = process.env.SERVICE_NAME || 'users';
 
 const DATABASE_URL = process.env.DATABASE_URL || 'postgres://user:pass@users-db:5432/users-db';
+const redisUrl = process.env.REDIS_URL || 'redis://redis:6379';
 
 const pool = new pg.Pool({ connectionString: DATABASE_URL });
 
@@ -14,7 +15,59 @@ pool.on('error', err => {
     console.error('Postgres error: ', err.message);
 });
 
+const client = redis.createClient({ url: redisUrl });
+
+client.on('error', err => {
+    console.error('Redis error: ', err.message);
+});
+
+await client.connect();
+
 app.use(express.json());
+
+app.get('/users', async (req, res) => {
+    // Users db intended for simulation and testing purposes, not actual user storage and sensitive information
+    const limit = Math.min(Math.max(parseInt(req.query.limit || "5", 10), 1), 100);
+    const cacheKey = `users:${limit}`;
+
+    try {
+        // Check if users list already cached in Redis
+        const cached = await client.get(cacheKey);
+
+        if (cached) {
+            return res.json(JSON.parse(cached));
+        }
+
+        let baseQuery = `FROM users`;
+        const params = [];
+        const conditions = [];
+
+        params.push(limit);
+
+        const dataQuery = `SELECT * ${baseQuery} ORDER BY RANDOM() LIMIT $${params.length}`;
+
+        const result = await pool.query(dataQuery, params);
+
+        const rows = result.rows;
+
+        const response = {
+            limit,
+            users: result.rows,
+        };
+
+        // Cache results in redis
+        await client.setEx(cacheKey, 60, JSON.stringify(response));
+
+        res.json(response);
+    } catch  (err) {
+        console.error("GET /users failed: ", err);
+
+        res.status(503).json({
+            error: "Database or Redis unavailable",
+            details: err.message,
+        });
+    }
+});
 
 app.get('/health', async (_req, res) => {
     let healthy = true;
