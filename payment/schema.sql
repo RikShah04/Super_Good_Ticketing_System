5ca2ed95a75ee76765aebb534c26d105b680fc7e
KingShibe <henry.w.branham@gmail.com>
2026-04-26 11:37:07 -0400

    Fixed status character limitation

--- payment/schema.sql
+++ payment/schema.sql
@@ -1,9 +1,11 @@
+DROP TABLE IF EXISTS payments;
+
 CREATE TABLE payments (
     payment_id    CHAR(36)       NOT NULL PRIMARY KEY,
     token           VARCHAR(255) NOT NULL, 
     price           DECIMAL(10, 2) NOT NULL, 
     amount_refunded           DECIMAL(10, 2) NOT NULL, 
-    status          VARCHAR(10) NOT NULL CHECK (status IN ('success', 'failure', 'refunded', 'partial_refund')), 
+    status          VARCHAR(20) NOT NULL CHECK (status IN ('success', 'failure', 'refunded', 'partial_refund')), 
     created_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
