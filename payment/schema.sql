19d325f5ffa7e3dfa0befac5c29d5b2eb6f94150
KingShibe <henry.w.branham@gmail.com>
2026-04-26 02:24:04 -0400

    Updated schema

--- payment/schema.sql
+++ payment/schema.sql
@@ -2,7 +2,8 @@ CREATE TABLE payments (
     payment_id    CHAR(36)       NOT NULL PRIMARY KEY,
     token           VARCHAR(255) NOT NULL, 
     price           DECIMAL(10, 2) NOT NULL, 
-    status          VARCHAR(10) NOT NULL CHECK (status IN ('success', 'failure', 'refunded')), 
+    amount_refunded           DECIMAL(10, 2) NOT NULL, 
+    status          VARCHAR(10) NOT NULL CHECK (status IN ('success', 'failure', 'refunded', 'partial_refund')), 
     created_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
